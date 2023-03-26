da3c68fe605da7bbfb3b99ef979b8787cd7b6ca7
gyungwan <zddx1994@gmail.com>
2023-03-26 12:33:38 +0900

    [ FEATURE ] 상품 스웨거 연결

--- src/apis/products/products.service.ts
+++ src/apis/products/products.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  //   create({ createProductInput }) {}
+}

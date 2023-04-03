c92f3a926a7e629177e073607d38c843dc3df2a6
Tom Ko <yungsu2391@gmail.com>
2023-04-04 08:20:04 +0900

    [ ADD ] 상품 카테고리 추가 ,상품 상태값으로 업데이트 가능여부 확인

--- src/apis/products/products.service.ts
+++ src/apis/products/products.service.ts
@@ -14,13 +14,21 @@ export class ProductsService {
 
   async create({ createProductInput }) {
     //이부분 코드 더 짜야하는데 공부하고 짤게요ㅠ
-    const result = await this.productRepository.save({ ...createProductInput });
+    const { productCategory, ...product } = createProductInput;
+    const result = await this.productRepository.save({
+      ...product,
+      productCategory: {
+        id: productCategory,
+      },
+    });
     return result;
   }
 
   async findAll() {
     // const result = await this.productRepository.find({where:{}});
-    const result = await this.productRepository.find();
+    const result = await this.productRepository.find({
+      relations: ['productCategory'],
+    });
     return result;
   }
 
@@ -32,9 +40,14 @@ export class ProductsService {
   }
 
   async update({ productId, updateProductInput }) {
+    // 상품의 업데이트는 상태값이 0인것만 업데이트 가능하다.(0 === 판매 대기중인 상태)
     const oldProduct = await this.productRepository.findOne({
       where: { id: productId },
     });
+    if (oldProduct.status !== 0) {
+      throw new Error('상품의 상태가 판매 대기중이 아닙니다.');
+    }
+
     const newProduct = {
       ...oldProduct,
       id: productId,
@@ -45,7 +58,38 @@ export class ProductsService {
   }
 
   async delete({ id }) {
-    const result = await this.productRepository.softDelete({ id });
-    return result.affected ? true : false;
+    await this.productRepository.update(id, { status: -1 });
+    const deleteResult = await this.productRepository.softDelete({ id });
+    return deleteResult.affected ? true : false;
+  }
+
+  async startStatus({ productId }) {
+    const oldProduct = await this.productRepository.findOne({
+      where: { id: productId },
+    });
+    if (oldProduct.status !== 0) {
+      throw new Error('상품의 상태가 판매 대기중이 아닙니다.');
+    }
+    const newProduct = {
+      ...oldProduct,
+      status: 1,
+    };
+    const result = await this.productRepository.save(newProduct);
+    return result;
+  }
+
+  async endStatus({ productId }) {
+    const oldProduct = await this.productRepository.findOne({
+      where: { id: productId },
+    });
+    if (oldProduct.status !== 1) {
+      throw new Error('상품의 상태가 판매중이 아닙니다.');
+    }
+    const newProduct = {
+      ...oldProduct,
+      status: 2,
+    };
+    const result = await this.productRepository.save(newProduct);
+    return result;
   }
 }

8674609e023b72159adcbae2dcfc6f19e7e2f020
김태현 <eiiyy12@nate.com>
2023-04-30 16:13:57 +0900

    [UPDATE] CORS 처리 추가

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.enableCors({
+    origin: ['https://price-crush-client.vercel.app', 'http://localhost:3000'],
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
